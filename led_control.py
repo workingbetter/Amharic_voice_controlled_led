f58435e0920bb98941d29d968be43632055f7989
Michael Bogale <127705286+workingbetter@users.noreply.github.com>
2024-09-13 19:55:27 +0100

    Add files via upload

--- led_control.py
+++ led_control.py
@@ -5,10 +5,10 @@ led = LED(17)  # define LED pin according to BCM Numbering
 
 command = sys.argv[1]  # Get the command from arguments
 
-if command.lower() == "on":
+if command == "በራ":
     led.on()
     print("LED turned on")
-elif command.lower() == "off":
+elif command == "ጠፋ":
     led.off()
     print("LED turned off")
 else:
