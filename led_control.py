7b4f7805517acdded8b10d817fb1a22afd694f73
Michael Bogale <127705286+workingbetter@users.noreply.github.com>
2024-09-13 20:53:26 +0100

    Add files via upload

--- led_control.py
+++ led_control.py
@@ -1,15 +1,21 @@
 from gpiozero import LED
 import sys
+import time
 
-led = LED(17)  # define LED pin according to BCM Numbering
-
-command = sys.argv[1]  # Get the command from arguments
-
-if command == "በራ":
-    led.on()
-    print("LED turned on")
-elif command == "ጠፋ":
-    led.off()
-    print("LED turned off")
-else:
-    print("Unknown command")
+def control_led(command):
+    led = LED(17)  # define LED pin according to BCM Numbering
+    if command == "በራ":
+        led.on()
+        print("LED turned on miki")
+    elif command == "ጠፋ":
+        led.off()
+        print("LED turned off")
+    else:
+        print("Unknown command")
+       
+    while True:
+        time.sleep(1)  # Delay to keep the script running
+       
+if __name__ == '__main__':
+    command = sys.argv[1]  # Get the command from arguments
+    control_led(command)
